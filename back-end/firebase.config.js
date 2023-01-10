0c66d509e2bcfb4c50dff76daf8993890bdb73d3
Long Huynh <long.huynh5713@gmail.com>
2023-01-10 10:52:22 +0200

    restructure the private files.

--- back-end/firebase.config.js
+++ back-end/firebase.config.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
+require('dotenv').config()
 
-const serviceAccount = require('./serviceAccount.json');
+
+const serviceAccount = JSON.parse(process.env['CREDS']);
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
