7b555c3aef8d4188fd4c694a0a8ce05bb14c9d46
Long Huynh <long.huynh5713@gmail.com>
2024-10-24 01:43:34 +0300

    Deleted unused files.

--- back-end/db/index.js
+++
@@ -1,7 +0,0 @@
-const pool = require('./config')
-
-module.exports = {
-    query: (text, params) => {
-        return pool.query(text, params)
-    }
-}
--- back-end/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const errorHandlerMiddleware = (err, req, res, next) => {
-    console.log(err.stack);
-    return res.status(500).json({ msg: "Internal errors" });
-};
-
-module.exports = errorHandlerMiddleware;
